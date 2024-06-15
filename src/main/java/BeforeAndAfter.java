b2252893e0400d22c9eb3da4fb50c1d32978937a
kritibhawsar1 <bhawsarkriti250@gmail.com>
2024-06-15 17:45:06 +0530

    practicing while, for loop along with arrays by writing test cases for filling form

--- src/main/java/BeforeAndAfter.java
+++ src/main/java/BeforeAndAfter.java
@@ -2,8 +2,11 @@ import org.junit.BeforeClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class BeforeAndAfter {
 
@@ -13,6 +16,10 @@ public class BeforeAndAfter {
 
     public String heading = "Complete Web Form";
 
+    public String[] names = {"Kriti", "Bhawsar", "SE"};
+
+    public String date = "31/12/2001";
+
     @FindBy(xpath = "//*[@class='container']/h1")
     public WebElement headingXpath;
 
@@ -20,4 +27,28 @@ public class BeforeAndAfter {
         return element.getText().trim();
     }
 
+    @FindAll({
+            @FindBy(xpath = "//*[@id='first-name']"),
+            @FindBy(xpath = "//*[@id='last-name']"),
+            @FindBy(xpath = "//*[@id='job-title']")
+    })
+    public List<WebElement> namesXpath;
+
+    @FindAll({
+            @FindBy(xpath = "//*[@id='radio-button-1']"),
+            @FindBy(xpath = "//*[@id='checkbox-2']")
+    })
+    public List<WebElement> radioButtonAndCheckBoxXpath;
+
+    @FindBy(xpath = "//*[@id='select-menu']")
+    public WebElement experienceDropdownXpath;
+
+    @FindBy(xpath = "//*[@value='1']")
+    public WebElement experienceValueXpath;
+
+    @FindBy(xpath = "//*[@id='datepicker']")
+    public WebElement datePickerXpath;
+
+    @FindBy(xpath = "//*[@role='button']")
+    public WebElement submitButtonXpath;
 }
--- src/test/java/BeforeAndAfterTest.java
+++ src/test/java/BeforeAndAfterTest.java
@@ -7,7 +7,7 @@ public class BeforeAndAfterTest extends BaseTest {
     BeforeAndAfter beforeAndAfter;
 
     @Parameters({"mainUrl"})
-    @Test(priority = 1)
+//    @Test(priority = 1)
     public void openUrl(String mainUrl) throws InterruptedException {
 
         beforeAndAfter = new BeforeAndAfter(startDriver());
@@ -18,8 +18,43 @@ public class BeforeAndAfterTest extends BaseTest {
         String s = beforeAndAfter.readText(beforeAndAfter.headingXpath);
         System.out.println(s);
         Assert.assertEquals(beforeAndAfter.heading, beforeAndAfter.readText(beforeAndAfter.headingXpath));
+    }
 
-        driver.quit();
+    @Parameters({"mainUrl"})
+    @Test(priority = 1)
+    public void fillingFormTest(String mainUrl) throws InterruptedException {
+
+        beforeAndAfter = new BeforeAndAfter(startDriver());
+        getDriver().manage().window().maximize();
+        getDriver().get(mainUrl);
+
+        System.out.println("Inserting name");
+        for(int i=0; i<beforeAndAfter.namesXpath.size(); i++)
+        {
+            beforeAndAfter.namesXpath.get(i).sendKeys(beforeAndAfter.names[i]);
+            Thread.sleep(200);
+        }
+
+        System.out.println("Selecting radio button and checkboxes");
+        int i=0;
+        while(i<beforeAndAfter.radioButtonAndCheckBoxXpath.size()){
+            beforeAndAfter.radioButtonAndCheckBoxXpath.get(i).click();
+            i++;
+            Thread.sleep(200);
+        }
+
+        System.out.println("Selecting experience from dropdown");
+        beforeAndAfter.experienceDropdownXpath.click();
+        beforeAndAfter.experienceValueXpath.click();
+        Thread.sleep(200);
+
+        System.out.println("Selecting date in date picker");
+        beforeAndAfter.datePickerXpath.sendKeys(beforeAndAfter.date);
+        Thread.sleep(400);
+
+        System.out.println("clicking submit button");
+        beforeAndAfter.submitButtonXpath.click();
+        
     }
 
 }
